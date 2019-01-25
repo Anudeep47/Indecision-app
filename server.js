9236e420d288e8a0c98efc08c7f032729b78aabe
Anudeep Pala <anudeep479@gmail.com>
2019-01-25 21:05:44 +0530

    heroku deployment fix

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, () => console.log(`App is running at ${PORT}`));
